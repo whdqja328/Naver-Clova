0ca832db0bb3962e0d7b97d928bf8aed76415405
whdqja328 <whdqja328@naver.com>
2021-06-12 09:13:30 +0900

    NaverClova

--- clova-app/clova-app.js
+++ clova-app/clova-app.js
@@ -1,7 +1,7 @@
 // Nav Menu
 const menuList = document.querySelectorAll('.menu_list > li');
 
-for (let i = 0, max = menuList.length; i<max; i++) {
+for (let i = 0, max = menuList.length; i < max; i++) {
     menuList[i].addEventListener('mouseenter', function (e) {
         e.target.classList.add('on');
     });
@@ -13,7 +13,6 @@ for (let i = 0, max = menuList.length; i<max; i++) {
 
 //Scroll Event
 const mainHeader = document.querySelector('.header_dark');
-
 
 let beforePosition = document.documentElement.scrollTop;
 
@@ -50,3 +49,75 @@ function fadeIn() {
     //     }
     // }
 };
+
+// Slider
+document.addEventListener("DOMContentLoaded", function () {
+    // 변수 지정
+    const slideWrap = document.querySelector(".img_area"),
+      slideContainer = document.querySelector(".info_list"),
+      slides = document.querySelectorAll(".info_list li"),
+      pagerBtn = document.querySelectorAll(".paginate a");
+  
+    let slideCount = slides.length,
+      currentIndex = 0,
+      timer = null;
+    
+    let slideWidth = 352;
+    // 슬라이드 가로로 배열하기
+    for (let i = 0; i < slideCount; i++) {
+      slides[i].style.left = i * 0 + "px";
+    }
+  
+    // 슬라이드 이동함수
+    function goToSlide(idx) {
+      slideContainer.classList.add("animate");
+      slideContainer.style.left = -slideWidth * idx + "px";
+      currentIndex = idx;
+  
+      for (let y = 0; y < pagerBtn.length; y++) {
+        pagerBtn[y].classList.remove("on");
+      }
+      pagerBtn[idx].classList.add("on");
+  
+      for (let s = 0; s < slideCount; s++) {
+        slides[s].classList.remove("fade_in");
+      }
+      slides[idx].classList.add("fade_in");
+    }
+  
+    goToSlide(0);
+  
+    //자동 슬라이드 함수
+    function startAutoSlide() {
+      timer = setInterval(function () {
+        let nextIdx = (currentIndex + 1) % slideCount;
+  
+        goToSlide(nextIdx);
+      }, 4500);
+    }
+  
+    startAutoSlide();
+  
+    // 슬라이드 정지 함수
+    function stopAutoSlide() {
+      clearInterval(timer);
+    }
+  
+    //slideWrap에 마우스가 들어오면 할일, 나가면 할일
+    slideWrap.addEventListener("mouseenter", function () {
+      stopAutoSlide();
+    });
+  
+    slideWrap.addEventListener("mouseleave", function () {
+      startAutoSlide();
+    });
+  
+    //pager로 슬라이드 이동하기
+    for (let a = 0; a < pagerBtn.length; a++) {
+      pagerBtn[a].addEventListener("click", function (e) {
+        e.preventDefault();
+        let pagerNum = e.target.getAttribute("data-idx");
+        goToSlide(pagerNum);
+      });
+    }
+  });
